9da4b9dcb401317546df090998520405f5b469d4
akecebasbenarif <kecebasbenarif12728@lasalle63.fr>
2023-03-10 15:46:41 +0100

    getMachines

--- src/controllers/MachineController.php
+++ src/controllers/MachineController.php
@@ -16,6 +16,9 @@ class MachineController extends IController {
             case 'delete':
                 $this->run_delete();
                 break;
+            case 'add':
+                $this->run_add();
+                break;
             default:
                 $this->run_default_case("Machine", "?uc=machine&action=index");
         }
@@ -38,4 +41,29 @@ class MachineController extends IController {
         }
     }
 
+//    private function run_add() {
+//        $code = filter_input(INPUT_POST, 'code', FILTER_SANITIZE_STRING);
+//        $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
+//        $mail = filter_input(INPUT_POST, 'mail', FILTER_SANITIZE_STRING);
+//        $password = filter_input(INPUT_POST, 'password1', FILTER_SANITIZE_STRING);
+//        $actif = filter_input(INPUT_POST, 'checkActive', FILTER_SANITIZE_STRING);
+//        $role = filter_input(INPUT_POST, 'checkAdmin', FILTER_SANITIZE_STRING);
+//        if (!$actif) {
+//            $actif = "0";
+//        }
+//        if (!$role) {
+//            $role = 'Utilisateur';
+//        }
+//        if ($mail) {
+//            $addInterv = IntervenantRepository::addinterv($nom, $prenom, $mail, $password, $actif, $role);
+//            if ($addInterv) {
+//                $intervenants = IntervenantRepository::getAll();
+//                $this->smarty->assign('intervenants', $intervenants);
+//                $this->smarty->display('intervenant/index_intervenant.tpl');
+//            } else {
+//                $this->display_info("Problème de bdd", "L'intervenant na pas pu être ajouté à la base", "index.php");
+//            }
+//        } else {
+//            $this->display_info("Problème d'url", "L'intervenant na pas pu être ajouté", "index.php");
+//        }
 }
--- src/repositories/MachineRepository.php
+++ src/repositories/MachineRepository.php
@@ -3,7 +3,7 @@
 namespace repositories;
 
 class MachineRepository {
- 
+
     public static function getMachines() {
         $sql = "SELECT * 
                 from  machine 
@@ -15,11 +15,24 @@ class MachineRepository {
         $ligne = $stmt->fetchAll();
         return $ligne;
     }
+
     public static function deleteMachines($code) {
-         $sql = "delete from machine  where code=:code";
+        $sql = "delete from machine  where code=:code";
         $stmt = PdoBD::getInstance()->getMonPdo()->prepare($sql);
         $stmt->bindParam(":code", $code);
         return $stmt->execute();
     }
-    
+
+    public static function addMachines($code, $site_uai, $date_fabrication, $numero_serie, $type_machine_code) {
+        $sql = "insert into machine(code, site_uai,date_fabrication,numero_serie,type_machine_code)values(:code, :site_uai, :date_fabrication, :numero_site,:type_machine_code)";
+        $stmt = PdoBD::getInstance()->getMonPdo()->prepare($sql);
+        $stmt->bindValue(":code", $code);
+        $stmt->bindValue(":site_uai", $site_uai);
+        $stmt->bindValue(":date_fabrication", $date_fabrication);
+        $stmt->bindValue(":numero_site", $numero_serie);
+        $stmt->bindValue(":type_machine_code", $type_machine_code);
+
+        return $stmt->execute();
+    }
+
 }
